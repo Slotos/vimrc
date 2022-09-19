ea864a10a12ea623f243498f90edf42e07e7459e
Slotos <slotos@gmail.com>
2022-09-19 13:14:50 +0300

    Revert transition to lualine tabs component
    First step to reverting back to lightline

--- lua/settings/lualine.lua
+++ lua/settings/lualine.lua
@@ -55,87 +55,87 @@ if vim.fn['pac#loaded']('lualine.nvim') then
       lualine_z = {}
     },
     tabline = {
-      lualine_a = {
-        {
-          'tabs',
-          max_length = vim.o.columns,
-          mode = 2,
-          icons_enabled = true,
-          modification_icons_enabled = true,
-          icon = {
-            align = 'right',
-            use_default = true
-          },
-        }
-      },
+      -- lualine_a = {
+      --   {
+      --     'tabs',
+      --     max_length = vim.o.columns,
+      --     mode = 2,
+      --     icons_enabled = true,
+      --     modification_icons_enabled = true,
+      --     icon = {
+      --       align = 'right',
+      --       use_default = true
+      --     },
+      --   }
+      -- },
     },
     extensions = { 'fugitive', 'quickfix', neoterm_extension },
   }
 
   require 'lualine'.setup(lualine_config)
 
-  -- if vim.fn['pac#loaded']('luatab.nvim') then
-  --   function _G.GenerateTabLineSeparatorHls()
-  --     local bg, fg, hl
-  --     local h = require'color_helpers'
-  --     local dict = {
-  --       {TabLine = 'TabLineSel'},
-  --       {TabLineSel = 'TabLine'},
-  --       {TabLine = 'TabLineFill'},
-  --       {TabLineSel = 'TabLineFill'},
-  --       }
+  if vim.fn['pac#loaded']('luatab.nvim') then
+    function _G.GenerateTabLineSeparatorHls()
+      local bg, fg, hl
+      local h = require'color_helpers'
+      local dict = {
+        {TabLine = 'TabLineSel'},
+        {TabLineSel = 'TabLine'},
+        {TabLine = 'TabLineFill'},
+        {TabLineSel = 'TabLineFill'},
+        }
 
-  --     for _, tab_transition in pairs(dict) do
-  --       for previous_tab_hl, next_tab_hl in pairs(tab_transition) do
-  --         fg = h.extract_highlight_colors(previous_tab_hl, 'bg') or 'NONE'
-  --         bg = h.extract_highlight_colors(next_tab_hl, 'bg') or 'NONE'
-  --         hl = h.create_component_highlight_group({bg = bg, fg = fg}, previous_tab_hl .. '_' .. next_tab_hl)
-  --       end
-  --     end
-  --   end
+      for _, tab_transition in pairs(dict) do
+        for previous_tab_hl, next_tab_hl in pairs(tab_transition) do
+          fg = h.extract_highlight_colors(previous_tab_hl, 'bg') or 'NONE'
+          bg = h.extract_highlight_colors(next_tab_hl, 'bg') or 'NONE'
+          hl = h.create_component_highlight_group({bg = bg, fg = fg}, previous_tab_hl .. '_' .. next_tab_hl)
+        end
+      end
+    end
 
-  --   vim.api.nvim_command('augroup TabLineSeparators')
-  --   vim.api.nvim_command('autocmd!')
-  --   vim.api.nvim_command('autocmd ColorScheme * call v:lua.GenerateTabLineSeparatorHls()')
-  --   vim.api.nvim_command('autocmd OptionSet background call v:lua.GenerateTabLineSeparatorHls()')
-  --   vim.api.nvim_command('augroup END')
+    vim.api.nvim_command('augroup TabLineSeparators')
+    vim.api.nvim_command('autocmd!')
+    vim.api.nvim_command('autocmd ColorScheme * call v:lua.GenerateTabLineSeparatorHls()')
+    vim.api.nvim_command('autocmd OptionSet background call v:lua.GenerateTabLineSeparatorHls()')
+    vim.api.nvim_command('augroup END')
 
-  --   local luatab_config = {
-  --     modified = function(bufnr)
-  --       local status = {}
+    local luatab_config = {
+      modified = function(bufnr)
+        local status = {}
 
-  --       if vim.fn.getbufvar(bufnr, '&modified') == 1 then table.insert(status, '\u{f448}') end
-  --       if vim.fn.getbufvar(bufnr, '&modifiable') ~= 1 then table.insert(status, '\u{f83d}') end
-  --       table.insert(status, '')
+        if vim.fn.getbufvar(bufnr, '&modified') == 1 then table.insert(status, '\u{f448}') end
+        if vim.fn.getbufvar(bufnr, '&modifiable') ~= 1 then table.insert(status, '\u{f83d}') end
+        table.insert(status, '')
 
-  --       return table.concat(status, ' ')
-  --     end,
-  --     windowCount = function(index) -- hijack to show tab number
-  --       return index .. '. '
-  --     end,
-  --     separator = function(index)
-  --       local fg, bg, hl
-  --       local separator_char = lualine_config.options.section_separators.left
-  --       local invert_separator = false
+        return table.concat(status, ' ')
+      end,
+      windowCount = function(index) -- hijack to show tab number
+        return index .. '. '
+      end,
+      separator = function(index)
+        local fg, bg, hl
+        local separator_char = lualine_config.options.section_separators.left
+        local invert_separator = false
 
-  --       if index == (vim.fn.tabpagenr() - 1) then -- inactive tab before active tab
-  --         hl = 'luatab_TabLine_TabLineSel'
-  --       elseif index == vim.fn.tabpagenr() and index == vim.fn.tabpagenr('$') then -- active final tab
-  --         hl = 'luatab_TabLineSel_TabLineFill'
-  --       elseif index == vim.fn.tabpagenr() then -- active but not final tab
-  --         hl = 'luatab_TabLineSel_TabLine'
-  --       elseif index == vim.fn.tabpagenr('$') then -- final inactive tab
-  --         hl = 'luatab_TabLine_TabLineFill'
-  --       else -- inactive tab before inactive tab
-  --         hl = 'TabLine'
-  --         separator_char = lualine_config.options.component_separators.left
-  --       end
+        if index == (vim.fn.tabpagenr() - 1) then -- inactive tab before active tab
+          hl = 'luatab_TabLine_TabLineSel'
+        elseif index == vim.fn.tabpagenr() and index == vim.fn.tabpagenr('$') then -- active final tab
+          hl = 'luatab_TabLineSel_TabLineFill'
+        elseif index == vim.fn.tabpagenr() then -- active but not final tab
+          hl = 'luatab_TabLineSel_TabLine'
+        elseif index == vim.fn.tabpagenr('$') then -- final inactive tab
+          hl = 'luatab_TabLine_TabLineFill'
+        else -- inactive tab before inactive tab
+          hl = 'TabLine'
+          separator_char = lualine_config.options.component_separators.left
+        end
 
-  --       -- return (index < vim.fn.tabpagenr('$') and '%#' .. hl .. '#' .. separator_char or '')
-  --       return '%#' .. hl .. '#' .. separator_char
-  --     end,
-  --     }
+        -- return (index < vim.fn.tabpagenr('$') and '%#' .. hl .. '#' .. separator_char or '')
+        return '%#' .. hl .. '#' .. separator_char
+      end,
+      }
 
-  --   require'luatab'.setup(luatab_config)
-  -- end
+    require'luatab'.setup(luatab_config)
+  end
 end
