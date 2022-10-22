dd395a677d5ee9366e86be1136d682dcc6d9ca2c
Slotos <slotos@gmail.com>
2022-10-22 17:02:21 +0300

    Beautify rose-pine telescope prompt
    Subjective!

--- lua/settings/colorschemes.lua
+++ lua/settings/colorschemes.lua
@@ -4,11 +4,14 @@ if vim.fn['pac#loaded']('rose-pine') then
       -- Borderless telescope
       TelescopeBorder = { fg = "overlay", bg = "overlay" },
       TelescopeNormal = { fg = "subtle", bg = "overlay" },
-      TelescopePromptNormal = { fg = "text", bg = "overlay" },
       TelescopeSelection = { fg = "text", bg = "highlight_med" },
+
       TelescopeTitle = { fg = "base", bg = "love" },
       TelescopePromptTitle = { fg = "base", bg = "pine" },
       TelescopePreviewTitle = { fg = "base", bg = "iris" },
+
+      TelescopePromptNormal = { fg = "text", bg = "surface" },
+      TelescopePromptBorder = { fg = "surface", bg = "surface" },
     },
   })
 end
