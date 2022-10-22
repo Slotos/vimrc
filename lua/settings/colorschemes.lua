ab1d5114a1ac03cc82fe3cdb5a9a4d1aff4e3fac
Slotos <slotos@gmail.com>
2022-10-22 16:57:32 +0300

    Configure catppuccin with borderless telescope
    Now that's a sentence

--- lua/settings/colorschemes.lua
+++ lua/settings/colorschemes.lua
@@ -12,3 +12,43 @@ if vim.fn['pac#loaded']('rose-pine') then
     },
   })
 end
+
+if vim.fn['pac#loaded']('catppuccin') then
+  (function()
+    local telescopeBorderless = function (flavor)
+      local cp = require("catppuccin.palettes").get_palette(flavor)
+
+      return {
+        TelescopeBorder = { fg = cp.surface0, bg = cp.surface0 },
+        TelescopeSelectionCaret = { fg = cp.flamingo },
+        TelescopeMatching = { fg = cp.peach },
+        TelescopeNormal = { bg = cp.surface0 },
+        TelescopeSelection = { fg = cp.text, bg = cp.surface1 },
+
+        TelescopeTitle = { fg = cp.crust, bg = cp.green },
+        TelescopePreviewTitle = { fg = cp.crust, bg = cp.red },
+        TelescopePromptTitle = { fg = cp.crust, bg = cp.mauve },
+
+        TelescopePromptNormal = { fg = cp.flamingo, bg = cp.crust },
+        TelescopePromptBorder = { fg = cp.crust, bg = cp.crust},
+      }
+    end
+
+    require("catppuccin").setup {
+      background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      term_colors = true,
+      dim_inactive = {
+        enabled = true
+      },
+      highlight_overrides = {
+        latte = telescopeBorderless('latte'),
+        frappe = telescopeBorderless('frappe'),
+        macchiato = telescopeBorderless('macchiato'),
+        mocha = telescopeBorderless('mocha'),
+      },
+    }
+  end)()
+end
