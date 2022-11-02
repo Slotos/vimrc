5cc1f4ece61630f2fd1c99ba62465f7f43957242
Slotos <slotos@gmail.com>
2022-11-02 15:04:13 +0200

    Only load git-conflict if it's detected

--- lua/settings/git_conflict.lua
+++ lua/settings/git_conflict.lua
@@ -1,4 +1,6 @@
-require('git-conflict').setup({
-  default_mappings = true,
-  disable_diagnostics = true,
-})
+if vim.fn['pac#loaded']('git-conflict.nvim') then
+  require('git-conflict').setup({
+    default_mappings = true,
+    disable_diagnostics = true,
+  })
+end
