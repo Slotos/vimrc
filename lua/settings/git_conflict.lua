1f8e8f0be28ea434b8f6261e8560ceedda99a8d0
Slotos <soap@slotos.net>
2022-11-17 00:07:01 +0200

    Refresh git-conflict setup on colorscheme change

--- lua/settings/git_conflict.lua
+++ lua/settings/git_conflict.lua
@@ -1,6 +1,20 @@
 if vim.fn['pac#loaded']('git-conflict.nvim') then
-  require('git-conflict').setup({
-    default_mappings = true,
-    disable_diagnostics = true,
-  })
+  local git_conflict = require('git-conflict')
+  local setupGitconflict = function()
+    git_conflict.setup({
+      default_mappings = true,
+      disable_diagnostics = true,
+    })
+  end
+
+  setupGitconflict()
+
+  vim.api.nvim_create_augroup('GitConflict', { clear = true })
+  vim.api.nvim_create_autocmd({ 'ColorScheme' },
+    {
+      group = 'GitConflict',
+      callback = setupGitconflict,
+      desc = 'Rerun GitConflict setup to update highlights',
+    }
+  )
 end
