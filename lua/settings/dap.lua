e8faca2fe9b6efd53f34528b51266622f6aa2fde
Slotos <soap@slotos.net>
2023-02-06 19:27:45 +0200

    Add bash and sh debugger configurations

--- lua/settings/dap.lua
+++ lua/settings/dap.lua
@@ -150,4 +150,33 @@ if vim.fn['pac#loaded']('nvim-dap') then
       name = "Attach to running Neovim instance",
     }
   }
+
+  dap.adapters.bashdb = {
+    type = 'executable',
+    command = 'bash-debug-adapter',
+    name = 'bashdb',
+  }
+
+  dap.configurations.sh = {
+  {
+    type = 'bashdb',
+    request = 'launch',
+    name = "Launch file",
+    showDebugOutput = true,
+    pathBashdb = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+    pathBashdbLib = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+    trace = true,
+    file = "${file}",
+    program = "${file}",
+    cwd = '${workspaceFolder}',
+    pathCat = "cat",
+    pathBash = "/usr/local/bin/bash",
+    pathMkfifo = "mkfifo",
+    pathPkill = "pkill",
+    args = {},
+    env = {},
+    terminalKind = "integrated",
+  }
+}
+
 end
