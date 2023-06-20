5fa108325249ab4dc3c018bd0ea651953b77ace8
Slotos <slotos@gmail.com>
2023-06-21 00:11:23 +0200

    Add Rails routes aerial query
    Not perfect, but good

--- after/queries/ruby/aerial.scm
+++ after/queries/ruby/aerial.scm
@@ -8,4 +8,24 @@
                       (string_content) @name)
                     (simple_symbol) @name
                     ])?
+      (#set! "kind" "Module")
+      ) @type
+
+;; Rails routes file
+(call receiver: (call) @receiver @name
+      (#match? @receiver "Rails.application.routes")
+      method: (identifier) @method @_
+      (#match? @method "draw")
+      (#set! "kind" "Module")
+      ) @type
+
+;; Ideally, this would be a nested query under the above, but (_)* still doesn't work in neovim
+(call method: (identifier) @method @name
+      (#any-of? @method "resources" "get" "post" "patch" "put" "delete" "root" "collection" "member")
+      arguments: (argument_list
+                   [(string
+                      (string_content) @name)
+                    (simple_symbol) @name
+                    ])?
+      (#set! "kind" "Method")
       ) @type
