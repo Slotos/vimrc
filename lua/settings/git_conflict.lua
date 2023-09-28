c042a58367b516010f1c2643ea530135824ff5fe
Slotos <slotos@gmail.com>
2023-09-28 18:03:57 +0200

    Update git conflict keybindings
    I couldn't find a way to make `vim.keymap.del` to silently do nothing if
    keymap in question doesn't exist. I'm not interested to know that the keymap
    didn't exist before the call, but rather that it definitely doesn't exist
    after.
    
    While not a big deal in a sequential execution, in an evented system I
    simply don't know whether the call to set up the keymaps was ever
    issued. Turns out, git-conflict can detect that the conflict is ongoing,
    but the resolution selection has already taken place. In which case it
    doesn't issue the event that leads to us setting up the keymaps.
    
    So in order to not see the errors in such a case, I'm looping over
    buffer keymaps and removing the ones that match git conflict right hand
    side.

--- lua/settings/git_conflict.lua
+++ lua/settings/git_conflict.lua
@@ -21,20 +21,21 @@ if vim.fn['pac#loaded']('git-conflict.nvim') then
     {
       group = 'GitConflict',
       pattern = 'GitConflictDetected',
-      callback = function()
+      callback = function(args)
+        local bufnr = args.buf
         local filename = vim.fn.expand('<afile>')
         vim.schedule(function()
           vim.notify('Conflict detected in ' .. filename, vim.log.levels.INFO,
             { title = 'GitConflict', icon = ' ' })
         end)
         vim.keymap.set('n', '<localleader>co', '<Plug>(git-conflict-ours)',
-          { buffer = true, desc = 'Git conflict: choose ours' })
+          { buffer = bufnr, desc = 'Git conflict: choose ours' })
         vim.keymap.set('n', '<localleader>ct', '<Plug>(git-conflict-theirs)',
-          { buffer = true, desc = 'Git conflict: choose theirs' })
+          { buffer = bufnr, desc = 'Git conflict: choose theirs' })
         vim.keymap.set('n', '<localleader>cb', '<Plug>(git-conflict-both)',
-          { buffer = true, desc = 'Git conflict: choose both' })
+          { buffer = bufnr, desc = 'Git conflict: choose both' })
         vim.keymap.set('n', '<localleader>c0', '<Plug>(git-conflict-none)',
-          { buffer = true, desc = 'Git conflict: choose none' })
+          { buffer = bufnr, desc = 'Git conflict: choose none' })
       end,
       desc = 'Setup buffer local git conflict keymaps when conflicts are detected',
     }
@@ -43,16 +44,22 @@ if vim.fn['pac#loaded']('git-conflict.nvim') then
     {
       group = 'GitConflict',
       pattern = 'GitConflictResolved',
-      callback = function()
+      callback = function(args)
+        local bufnr = args.buf
         local filename = vim.fn.expand('<afile>')
         vim.schedule(function()
           vim.notify('All conflicts resolved in ' .. filename, vim.log.levels.INFO,
             { title = 'GitConflict', icon = ' ' })
         end)
-        vim.keymap.del('n', '<localleader>co', { buffer = true })
-        vim.keymap.del('n', '<localleader>ct', { buffer = true })
-        vim.keymap.del('n', '<localleader>cb', { buffer = true })
-        vim.keymap.del('n', '<localleader>c0', { buffer = true })
+        for _, mapping in pairs(vim.api.nvim_buf_get_keymap(bufnr, "n")) do
+          if mapping.rhs and string.find(mapping.rhs, "<Plug>(git-conflict") == 1 then
+            vim.keymap.del("n", mapping.lhs, { buffer = bufnr })
+          end
+        end
+        -- vim.keymap.del('n', '<localleader>co', { silent = true, buffer = bufnr })
+        -- vim.keymap.del('n', '<localleader>ct', { silent = true, buffer = bufnr })
+        -- vim.keymap.del('n', '<localleader>cb', { silent = true, buffer = bufnr })
+        -- vim.keymap.del('n', '<localleader>c0', { silent = true, buffer = bufnr })
       end,
       desc = 'Remove buffer local git conflict keymaps when conflicts are resolved',
     }
