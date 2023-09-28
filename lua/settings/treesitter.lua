3da9aea5f3d9367af4f73485f68768d1692c1a61
Slotos <slotos@gmail.com>
2023-09-28 20:26:17 +0200

    Cleanup TS and bodge together :Inspect in a popup
    I'm gradually preparing for neovim 0.10. As a part of that, I wanted to
    get :Inspect in a float, which led me to copying a hell of a lot of code
    from :Inspect implementation to populate the floating window of my
    liking.
    
    I can probably do it better now that I've done it once, will try again
    later.

--- lua/settings/treesitter.lua
+++ lua/settings/treesitter.lua
@@ -1,144 +1,249 @@
+-- stolen from runtime/lua/vim/_inspector.lua
+local read_pos = function()
+  local items = vim.inspect_pos()
+
+  local lines = { {} }
+
+  local function append(str, hl)
+    table.insert(lines[#lines], { str, hl })
+  end
+
+  local function nl()
+    table.insert(lines, {})
+  end
+
+  local function item(data, comment)
+    append('  - ')
+    append(data.hl_group, data.hl_group)
+    append(' ')
+    if data.hl_group ~= data.hl_group_link then
+      append('links to ', 'MoreMsg')
+      append(data.hl_group_link, data.hl_group_link)
+      append(' ')
+    end
+    if comment then
+      append(comment, 'Comment')
+    end
+    nl()
+  end
+
+  -- treesitter
+  if #items.treesitter > 0 then
+    append('Treesitter', 'Title')
+    nl()
+    for _, capture in ipairs(items.treesitter) do
+      item(capture, capture.lang)
+    end
+    nl()
+  end
+
+  -- semantic tokens
+  if #items.semantic_tokens > 0 then
+    append('Semantic Tokens', 'Title')
+    nl()
+    local sorted_marks = vim.fn.sort(items.semantic_tokens, function(left, right)
+      local left_first = left.opts.priority < right.opts.priority
+        or left.opts.priority == right.opts.priority and left.opts.hl_group < right.opts.hl_group
+      return left_first and -1 or 1
+    end)
+    for _, extmark in ipairs(sorted_marks) do
+      item(extmark.opts, 'priority: ' .. extmark.opts.priority)
+    end
+    nl()
+  end
+
+  -- syntax
+  if #items.syntax > 0 then
+    append('Syntax', 'Title')
+    nl()
+    for _, syn in ipairs(items.syntax) do
+      item(syn)
+    end
+    nl()
+  end
+
+  -- extmarks
+  if #items.extmarks > 0 then
+    append('Extmarks', 'Title')
+    nl()
+    for _, extmark in ipairs(items.extmarks) do
+      if extmark.opts.hl_group then
+        item(extmark.opts, extmark.ns)
+      else
+        append('  - ')
+        append(extmark.ns, 'Comment')
+        nl()
+      end
+    end
+    nl()
+  end
+
+  if #lines[#lines] == 0 then
+    table.remove(lines)
+  end
+
+  local chunks = {}
+  for _, line in ipairs(lines) do
+    vim.list_extend(chunks, line)
+    table.insert(chunks, { '\n' })
+  end
+
+  return chunks
+end
+
+local local_ts_namespace = vim.api.nvim_create_namespace("")
+
+vim.keymap.set(
+  {"n"},
+  "<localleader>h",
+  function()
+    -- @todo format into a floating window (nvim-open-win)
+    local inspected = read_pos()
+
+    if #inspected == 0 then return end
+
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    if bufnr == 0 then return end
+
+    local col, row = 0, 0
+    local max_width, width = 0, 0
+
+    for _, chunk in ipairs(inspected) do
+      local text, highlight = unpack(chunk)
+      local text_len = string.len(text)
+      vim.print(text)
+
+      if text == "\n" then
+        max_width = math.max(width, max_width)
+        width = 0
+
+        col = 0
+        row = row + 1
+
+        -- insert a new line
+        vim.api.nvim_buf_set_lines(bufnr, row, row, false, {""})
+      else
+        width = width + text_len
+
+        local new_col = col + text_len
+
+        vim.print({col, new_col})
+        vim.api.nvim_buf_set_text(bufnr, row, col, row, col, { text })
+
+        if highlight then
+          vim.api.nvim_buf_add_highlight(bufnr, local_ts_namespace, highlight, row, col, new_col)
+        end
+
+        col = new_col
+      end
+    end
+
+    local winnr = vim.api.nvim_open_win(bufnr, false, {
+      relative = "cursor", row = 1, col = 0,
+      width = max_width, height = row + 1, style = "minimal",
+      border = {" ", " " ," ", " ", " ", " ", " ", " "},
+      focusable = false, noautocmd = true
+    })
+
+    if winnr == 0 then return end
+    vim.print(winnr)
+
+    -- Ensure we close the window when moving around
+    local augroup = vim.api.nvim_create_augroup('preview_window_' .. winnr, {
+      clear = true,
+    })
+
+    local lookup_bufnr = vim.api.nvim_get_current_buf()
+
+    -- close the preview window when entered a buffer that is not
+    -- the floating window buffer or the buffer that spawned it
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = augroup,
+      callback = function(args)
+        if args.buf == bufnr or args.buf == lookup_bufnr then return end
+
+        vim.api.nvim_del_augroup_by_id(augroup)
+        pcall(vim.api.nvim_win_close, winnr, true)
+      end,
+    })
+
+    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI', 'InsertCharPre' }, {
+      group = augroup,
+      buffer = lookup_bufnr,
+      callback = function()
+        vim.api.nvim_del_augroup_by_id(augroup)
+        pcall(vim.api.nvim_win_close, winnr, true)
+      end,
+    })
+  end,
+  { silent = true, desc = "Inspect contents at the cursor position" }
+)
+
+local treesitter_grp = vim.api.nvim_create_augroup('Treesitter', {clear = true})
+vim.api.nvim_create_autocmd('FileType', {
+  group = treesitter_grp,
+  callback = function() pcall(vim.treesitter.start) end,
+})
+
 if vim.fn['pac#loaded']('nvim-treesitter') then
+  if vim.fn['pac#loaded']('nvim-ts-context-commentstring') then
+    require('ts_context_commentstring').setup {}
+  end
+
   require'nvim-treesitter.configs'.setup {
-    highlight = {
+    -- highlight = {
+    --   enable = true,
+    -- },
+    indent = {
       enable = true,
     },
     matchup = {
       enable = true,
       include_match_words = false,
     },
-    context_commentstring = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-    },
     endwise = {
-      enable = true,
-    },
-    playground = {
-      enable = true,
-      disable = {},
-      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-      persist_queries = false, -- Whether the query persists across vim sessions
-      keybindings = {
-        toggle_query_editor = 'o',
-        toggle_hl_groups = 'i',
-        toggle_injected_languages = 't',
-        toggle_anonymous_nodes = 'a',
-        toggle_language_display = 'I',
-        focus_language = 'f',
-        unfocus_language = 'F',
-        update = 'R',
-        goto_node = '<cr>',
-        show_help = '?',
-      },
-    },
-    query_linter = {
-      enable = true,
-      use_virtual_text = true,
-      lint_events = {"BufWrite", "CursorHold"},
-    },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-    },
-    textobjects = {
-      select = {
-        enable = true,
-
-        -- Automatically jump forward to textobj, similar to targets.vim
-        lookahead = true,
-
-        keymaps = {
-          ["af"] = { query = "@function.outer", desc = "Select outer part of a function region"},
-          ["if"] = { query = "@function.inner", desc = "Select inner part of a function region"},
-          ["ac"] = { query = "@class.outer", desc = "Select outer part of a class region"},
-          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-          ["ap"] = { query = "@parameter.outer", desc = "Select outer part of a parameter region"},
-          ["ip"] = { query = "@parameter.inner", desc = "Select inner part of a parameter region" },
-          ["ab"] = { query = "@block.outer", desc = "Select outer part of a block region"},
-          ["ib"] = { query = "@block.inner", desc = "Select inner part of a block region" },
-        },
-        -- You can choose the select mode (default is charwise 'v')
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * method: eg 'v' or 'o'
-        -- and should return the mode ('v', 'V', or '<c-v>') or a table
-        -- mapping query_strings to modes.
-        selection_modes = {
-          ['@parameter.outer'] = 'v', -- charwise
-          ['@function.outer'] = 'V', -- linewise
-        },
-        -- If you set this to `true` (default is `false`) then any textobject is
-        -- extended to include preceding or succeeding whitespace. Succeeding
-        -- whitespace has priority in order to act similarly to eg the built-in
-        -- `ap`.
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * selection_mode: eg 'v'
-        -- and should return true of false
-        -- include_surrounding_whitespace = true,
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<localleader>p"] = "@parameter.inner",
-          ["<localleader>m"] = "@function.outer",
-        },
-        swap_previous = {
-          ["<localleader>P"] = "@parameter.inner",
-          ["<localleader>M"] = "@function.outer",
-        },
-      },
-    },
-    autotag = {
       enable = true,
     },
   }
 
-  vim.api.nvim_create_augroup('TreeSitterFolds', { clear = true })
   vim.api.nvim_create_autocmd({ 'FileType' },
     {
       pattern = 'ruby,eruby,javascript,lua,go,elixir,vim,typescript,markdown,typescriptreact',
-      group = 'TreeSitterFolds',
+      group = treesitter_grp,
       callback = function()
         vim.wo.foldmethod = 'expr'
         vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
       desc = 'Set tree-sitter folding for chosen filetypes',
     }
   )
 
-  vim.api.nvim_set_keymap('n', '<leader>h', ":TSHighlightCapturesUnderCursor<CR>", { silent = true })
-end
-
-if vim.fn['pac#loaded']('nvim-treesitter-context') then
-  require'treesitter-context'.setup{
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-    min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-      -- For all filetypes
-      -- Note that setting an entry here replaces all other patterns for this entry.
-      -- By setting the 'default' entry below, you can control which nodes you want to
-      -- appear in the context window.
-      ruby = {
-        'block',
-        'elsif',
-        'module',
-        'when',
+  if vim.fn['pac#loaded']('nvim-treesitter-context') then
+    require'treesitter-context'.setup{
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        -- For all filetypes
+        -- Note that setting an entry here replaces all other patterns for this entry.
+        -- By setting the 'default' entry below, you can control which nodes you want to
+        -- appear in the context window.
+        ruby = {
+          'block',
+          'elsif',
+          'module',
+          'when',
+        },
+        lua = {
+          'elseif_statement',
+          'else_statement',
+        },
+        html = {
+          'element',
+        },
       },
-      lua = {
-        'elseif_statement',
-        'else_statement',
-      },
-      html = {
-        'element',
-      },
-    },
-  }
+    }
+  end
 end
