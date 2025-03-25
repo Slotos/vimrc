fe4024bda1a59e1b07a5a2c12f7f0939745fe66e
Slotos <soap@slotos.net>
2025-03-25 21:01:29 +0100

    Update aerial config and source
    I still can't remove filesystem checks around telescope source,
    but it's due to telescope design issues at this point.

--- lua/settings/aerial.lua
+++ lua/settings/aerial.lua
@@ -1,46 +1,45 @@
-if vim.fn['pac#loaded']('aerial.nvim') then
-  vim.g.aerial_nvim_config = {
-    -- Priority list of preferred backends for aerial.
-    -- This can be a filetype map (see :help aerial-filetype-map)
-    backends = {
-      -- This underscore key is the default
-      ['_']  = {"lsp", "treesitter", "markdown", "man"},
-      ruby = {"treesitter", "lsp"},
-      html = {"treesitter", "lsp"},
-      lua = {"treesitter", "lsp"},
-    },
+vim.g.aerial_nvim_config = {
+  -- Priority list of preferred backends for aerial.
+  -- This can be a filetype map (see :help aerial-filetype-map)
+  backends = {
+    -- This underscore key is the default
+    ['_']  = {"lsp", "treesitter", "markdown", "man"},
+    ruby = {"treesitter", "lsp"},
+    html = {"treesitter", "lsp"},
+    lua = {"treesitter", "lsp"},
+    python = {"treesitter", "lsp"},
+  },
 
-    -- A list of all symbols to display. Set to false to display all symbols.
-    -- This can be a filetype map (see :help aerial-filetype-map)
-    -- To see all available values, see :help SymbolKind
-    filter_kind = false,
-    -- filter_kind = {
-    --   "Class",
-    --   "Module",
-    --   "Constant",
-    --   "Constructor",
-    --   "Enum",
-    --   "Function",
-    --   "Interface",
-    --   "Method",
-    --   "Struct",
-    --   "Field",
-    --   "Property",
-    --   "Label",
-    -- },
-  }
+  -- A list of all symbols to display. Set to false to display all symbols.
+  -- This can be a filetype map (see :help aerial-filetype-map)
+  -- To see all available values, see :help SymbolKind
+  filter_kind = false,
+  -- filter_kind = {
+  --   "Class",
+  --   "Module",
+  --   "Constant",
+  --   "Constructor",
+  --   "Enum",
+  --   "Function",
+  --   "Interface",
+  --   "Method",
+  --   "Struct",
+  --   "Field",
+  --   "Property",
+  --   "Label",
+  -- },
+}
 
-  vim.api.nvim_set_keymap('n', '<localleader>]', '<cmd>AerialToggle!<CR>', {})
-  -- Jump forwards/backwards with '{' and '}'
-  vim.api.nvim_set_keymap('n', '{', '<cmd>AerialPrev<CR>', {})
-  vim.api.nvim_set_keymap('n', '}', '<cmd>AerialNext<CR>', {})
-  -- Jump up the tree with '[[' or ']]'
-  vim.api.nvim_set_keymap('n', '[[', '', { callback = require'aerial'.prev_up })
-  vim.api.nvim_set_keymap('n', ']]', '', { callback = require'aerial'.next_up })
+vim.api.nvim_set_keymap('n', '<localleader>]', '<cmd>AerialToggle!<CR>', {})
+-- Jump forwards/backwards with '{' and '}'
+vim.api.nvim_set_keymap('n', '{', '<cmd>AerialPrev<CR>', {})
+vim.api.nvim_set_keymap('n', '}', '<cmd>AerialNext<CR>', {})
+-- Jump up the tree with '[[' or ']]'
+vim.api.nvim_set_keymap('n', '[[', '', { callback = function() require'aerial'.prev_up() end })
+vim.api.nvim_set_keymap('n', ']]', '', { callback = function() require'aerial'.next_up() end })
 
-  if vim.fn['pac#loaded']('telescope.nvim') then
-    -- Open Telescope list
-    require('telescope').load_extension('aerial')
-    vim.api.nvim_set_keymap('n', '<localleader>[', '<cmd>Telescope aerial<CR>', {})
-  end
+if vim.fn['pac#loaded']('telescope.nvim') and vim.fn['pac#loaded']('aerial.nvim') then
+  -- Open Telescope list
+  require('telescope').load_extension('aerial')
+  vim.api.nvim_set_keymap('n', '<localleader>[', '<cmd>Telescope aerial<CR>', {})
 end
--- pac.vim
+++ pac.vim
@@ -17,7 +17,7 @@ endfunction
 " Tree-sitter
 call minpac#add('nvim-treesitter/nvim-treesitter', { 'branch': 'main', 'do': 'packloadall! | TSUpdate' })
 call minpac#add('nvim-treesitter/nvim-treesitter-context')
-call minpac#add('Slotos/aerial.nvim', { 'branch': 'be-a-plugin' })
+call minpac#add('Slotos/aerial.nvim')
 
 " LSP
 call minpac#add('neovim/nvim-lspconfig')
